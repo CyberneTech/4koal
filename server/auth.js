25751958a990b60aa2953afccbf41976f86488a3
Malika01 <malika.chikara@gmail.com>
2022-04-17 10:57:20 +0530

    auth - not working tho

--- server/auth.js
+++ server/auth.js
@@ -4,29 +4,33 @@ const jwt = require('jsonwebtoken');
   
 const app = express();
   
-// Set up Global configuration access
 dotenv.config();
-  
-/*let PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is up and running on ${PORT} ...`);
-});*/
-  
+
+// token generation
 app.post("/generateToken", (req, res) => {
   
     let jwtSecretKey = process.env.JWT_SECRET_KEY;
-    let data = {
-        userID: 1001,
-        password: 'something',
-        Role: 'Guest'
+    /* schema of user ->
+    {
+    userID : "",
+    secretKey: ""
     }
-  
-    const token = jwt.sign(data, jwtSecretKey);
-  
+    */
+    
+    let data = {
+        userID: req.body.userID,
+        secretKey: req.body.secretKey
+        //password: req.body.password,
+        //role: req.body.role
+    }
+
+    //let { id, pass, role } = req.body;
+
+    const token = jwt.sign(data, jwtSecretKey);  
     res.send(token);
 });
   
-// Verification of JWT
+// token validation 
 app.get("/validateToken", (req, res) => {
   
     let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
@@ -39,11 +43,9 @@ app.get("/validateToken", (req, res) => {
         if(verified){
             return res.send("Successfully Verified");
         }else{
-            // Access Denied
             return res.status(401).send(error);
         }
     } catch (error) {
-        // Access Denied
         return res.status(401).send(error);
     }
 });
